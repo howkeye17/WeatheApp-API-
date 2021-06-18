f45f668c0292c10e8e6b301eb5459e54cef1a6c2
howkeye17 <howkeye17@gmail.com>
2021-06-18 18:53:16 +0300

    struct for CurrentWeatherData Parse JSON

---
+++ WeatheApp (work with API)/Models/CurrentWeatherData.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentWeatherData.swift
+//  WeatheApp (work with API)
+//
+//  Created by Valera Vasilevich on 18.06.21.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Codable  {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
