c7c6ee8c5f1a54779958ea97609254e24d51bfda
howkeye17 <howkeye17@gmail.com>
2021-07-06 18:49:40 +0300

    work with updating UI

--- WeatheApp (work with API)/Models/NetworkWaetherManager.swift
+++ WeatheApp (work with API)/Models/NetworkWaetherManager.swift
@@ -12,7 +12,7 @@ class NetworkWaetherManager {
     var onCompletion: ((CurrentWeather) -> Void)?
     
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
         
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
--- WeatheApp (work with API)/View Controllers/ViewController.swift
+++ WeatheApp (work with API)/View Controllers/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     var networkWeatherManager = NetworkWaetherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
             self.networkWeatherManager.fetchCurrentWeather(forCity: city)
         }
     }
@@ -26,13 +26,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        networkWeatherManager.onCompletion = { currentWeather in
-            print(currentWeather.cityName)
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterfaceWith(weather: currentWeather)
         }
         networkWeatherManager.fetchCurrentWeather(forCity: "London")
         
     }
     
+    func updateInterfaceWith(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
+    }
     
 }
 
